0b19bdfd048a16a9b474de9b2be5b1b41720bc41
Niall O'Connor <zechs.marquie@gmail.com>
2014-08-14 14:32:22 +0100

    Encode rules and adopt a new approach

--- puzzles/einstein/solve_einstein.py
+++ puzzles/einstein/solve_einstein.py
@@ -1,53 +1,160 @@
 import einstein
+from pprint import pprint
+from copy import deepcopy
 
-def solve():
+def rule1(state):
     """
-    4  The Brit lives in a red house
-    5  The Swede keeps dogs
-    6  The Dane drinks tea
-    -  The green house is on the left of the white house
-    7  The green house owner drinks coffee
-    10 The person who plays polo rears birds
-    -  The owner of the yellow house plays hockey
-    3  The man living in the house right in the center drinks milk
-    1  The Norwegian lives in the first house
-    -- The man who plays baseball lives next to the man who keeps cats
-    -- The man who keeps horses lives next to the one who plays hockey
-    9  The man who plays billiards drinks beer
-    8  The German plays soccer
-    2  The Norwegian lives next to the blue house
-    -- The man who plays baseball has a neighbor who drinks water
+    The Brit lives in a red house.
     """
-    from pprint import pprint
+    return einstein.propose_value('house_color', 'red', 'nationality', 'british', state)
 
-    new_state = einstein.assign_value('1', 'nationality', 'norweigen', einstein.START_STATE)
-    houses = einstein.next_to(einstein.get_position('norweigen', new_state))
-    if len(houses) > 1:
-        new_state = einstein.propose_house(houses, 'house_color', 'blue', new_state)
+def rule2(state):
+    """
+    The Swede keeps dogs.
+    """
+    return einstein.propose_value('nationality', 'swedish', 'pet', 'dog', state)
+
+def rule3(state):
+    """
+    The Dane drinks tea.
+    """
+    return einstein.propose_value('nationality', 'dane', 'drink', 'tea', state)
+
+def rule4(state):
+    """
+    The green house is on the left of the white house.
+    """
+    house = einstein.get_position('green', state)
+    if house:
+        return einstein.assign_value(einstein.left_of(house), 'house_color', 'white', state)
     else:
-        new_state = einstein.assign_value(houses[0], 'house_color', 'blue', new_state)
-    new_state = einstein.elimination_sweep(new_state)
-    new_state = einstein.assign_value('3', 'drink', 'milk', new_state)
-    new_state = einstein.elimination_sweep(new_state)
-    new_state = einstein.propose_value('house_color', 'red', 'nationality', 'british', new_state)
-    new_state = einstein.elimination_sweep(new_state)
-    new_state = einstein.propose_value('nationality', 'swedish', 'pet', 'dog', new_state)
-    new_state = einstein.elimination_sweep(new_state)
-    new_state = einstein.propose_value('nationality', 'danish', 'drink', 'tea', new_state)
-    new_state = einstein.elimination_sweep(new_state)
-    new_state = einstein.propose_value('house_color', 'green', 'drink', 'coffee', new_state)
-    new_state = einstein.elimination_sweep(new_state)
-    new_state = einstein.propose_value('nationality', 'german', 'sport', 'soccer', new_state)
-    new_state = einstein.elimination_sweep(new_state)
-    new_state = einstein.propose_value('sport', 'billiards', 'drink', 'beer', new_state)
-    new_state = einstein.elimination_sweep(new_state)
-    new_state = einstein.propose_value('sport', 'polo', 'pet', 'bird', new_state)
-    new_state = einstein.elimination_sweep(new_state)
-    new_state = einstein.propose_value('sport', 'polo', 'pet', 'bird', new_state)
-    new_state = einstein.elimination_sweep(new_state)
+        return state
 
-    pprint(new_state)
-    einstein.print_solution(new_state)
+def rule5(state):
+    """
+    The green house owner drinks coffee.
+    """
+    house = einstein.get_position('green', state)
+    if house:
+        return einstein.assign_value(house, 'drink', 'coffee', state)
+    else:
+        return state
+
+def rule6(state):
+    """
+    The person who plays polo rears birds.
+    """
+    return einstein.propose_value('sport', 'polo', 'pet', 'bird', state)
+
+def rule7(state):
+    """
+    The owner of the yellow house plays hockey.
+    """
+    return einstein.propose_value('house_color', 'yellow', 'sport', 'hockey', state)
+
+def rule8(state):
+    """
+    The man living in the house right in the center drinks milk.
+    """
+    return einstein.assign_value('3', 'drink', 'milk', state)
+    
+def rule9(state):
+    """
+    The Norwegian lives in the first house.
+    """
+    return einstein.assign_value('1', 'nationality', 'norweigen', state)
+
+def rule10(state):
+    """
+    The man who plays baseball lives next to the man who keeps cats.
+    """
+    baseball_house = einstein.get_position('baseball', state)
+    cat_house = einstein.get_position('cat', state)
+    if cat_house and baseball_house: # We have assigned these rules already
+        return state
+    elif cat_house: # We only know where the cat lives
+        return einstein.propose_house(einstein.next_of(cat_house), 'sport', 'baseball', state)
+    elif baseball_house:# we only know where the baseball lives
+        return einstein.propose_house(einstein.next_of(baseball_house), 'pet', 'cat', state)
+    else: # We don't know where either the baseball or cat live
+        return state
+
+def rule11(state):
+    """
+    The man who keeps horses lives next to the one who plays hockey.
+    """
+    hockey_house = einstein.get_position('hockey', state)
+    horse_house = einstein.get_position('horse', state)
+    if horse_house and hockey_house: # We have assigned these rules already
+        return state
+    elif horse_house: # We only know where the horse lives
+        return einstein.propose_house(einstein.next_of(horse_house), 'sport', 'hockey', state)
+    elif hockey_house:# we only know where the hockey lives
+        return einstein.propose_house(einstein.next_of(hockey_house), 'pet', 'horse', state)
+    else: # We don't know where either the hockey or horse live
+        return state
+
+def rule12(state):
+    """
+    The man who plays billiards drinks beer.
+    """
+    return einstein.propose_value('sport', 'billiards', 'drink', 'beer', state)
+
+def rule13(state):
+    """
+    The German plays soccer.
+    """
+    return einstein.propose_value('nationality', 'german', 'sport', 'soccer', state)
+
+def rule14(state):
+    """
+    The Norwegian lives next to the blue house.
+    """
+    norweigen = einstein.get_position('norweigen', state)
+    if not norweigen:
+        return state
+    else:
+        houses = einstein.next_to(norweigen)
+        if len(houses) == 1:
+            return einstein.assign_value('2', 'house_color', 'blue', state)
+        else:
+            return einstein.propose_house(houses, 'house_color', 'blue', state)
+
+def rule15(state):
+    """
+    The man who plays baseball has a neighbor who drinks water.
+    """
+    baseball_house = einstein.get_position('baseball', state)
+    water_house = einstein.get_position('water', state)
+    if baseball_house and water_house: # We have assigned these rules already
+        return state
+    elif water_house: # We only know where the water lives
+        return einstein.propose_house(einstein.next_of(water_house), 'sport', 'baseball', state)
+    elif baseball_house:# we only know where the baseball lives
+        return einstein.propose_house(einstein.next_of(baseball_house), 'drink', 'water', state)
+    else: # We don't know where either the baseball or water live
+        return state
+
+def solve(state):
+    """
+
+    """
+    rule_list = [
+        rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8,
+        rule9, rule10, rule11, rule12, rule13, rule14, rule15
+    ]
+    for puzzle_iteration in xrange(100):
+        for rule in rule_list:
+            state = einstein.elimination_sweep(rule(state))
+            if einstein.end_solution(state):
+                return state, puzzle_iteration
+    return state, puzzle_iteration
+        
+           
 
 if __name__ == '__main__':
-    solve()
+    state = deepcopy(einstein.START_STATE)
+    state, puzzle_iteration = solve(state)
+    pprint(state)
+    print "Stopped at iteration {0}".format(puzzle_iteration)
+    einstein.print_solution(state)
