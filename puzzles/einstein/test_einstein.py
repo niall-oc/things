c6df944aa4188747e3f30b9147658d570381014a
Niall O'Connor <zechs.marquie@gmail.com>
2014-08-14 20:41:50 +0100

    Update test_einstein.py

--- puzzles/einstein/test_einstein.py
+++ puzzles/einstein/test_einstein.py
@@ -1,155 +1,257 @@
-import unittest
-import einstein
-from copy import deepcopy
+from   copy import deepcopy
+import logging
+import collections
 
-class EinsteinTest(unittest.TestCase):
+logging.basicConfig(level=logging.ERROR)
+
+house_colors =  ['red',     'green',  'blue',     'white',   'yellow']
+nationalities = ['british', 'danish', 'german',   'swedish', 'norweigen']
+drinks =        ['tea',     'coffee', 'milk',     'beer',    'water']
+sports =        ['polo',    'hockey', 'baseball', 'soccer',  'billiards']
+pets =          ['dog',     'cat',    'horse',    'bird',    'fish']
+
+
+START_STATE = collections.OrderedDict()
+START_STATE['1'] = {
+    'house_color': set(house_colors), 'nationality': set(nationalities),
+    'drink': set(drinks), 'sport': set(sports), 'pet': set(pets)
+}
+START_STATE['2'] = {
+    'house_color': set(house_colors), 'nationality': set(nationalities),
+    'drink': set(drinks), 'sport': set(sports), 'pet': set(pets)
+}
+START_STATE['3'] = {
+    'house_color': set(house_colors), 'nationality': set(nationalities),
+    'drink': set(drinks), 'sport': set(sports), 'pet': set(pets)
+}
+START_STATE['4'] = {
+    'house_color': set(house_colors), 'nationality': set(nationalities),
+    'drink': set(drinks), 'sport': set(sports), 'pet': set(pets)
+}
+START_STATE['5'] = {
+    'house_color': set(house_colors), 'nationality': set(nationalities),
+    'drink': set(drinks), 'sport': set(sports), 'pet': set(pets)
+} 
+        
+
+class StateError(Exception):
+    pass
+
+
+
+def print_solution(state):
     """
-    We need to assert who owns the Fish.
-
-    1  The Brit lives in a red house
-    2  The Swede keeps dogs
-    3  The Dane drinks tea
-    4  The green house is on the left of the white house
-    5  The green house owner drinks coffee
-    6  The person who plays polo rears birds
-    7  The owner of the yellow house plays hockey
-    8  The man living in the house right in the center drinks milk
-    9  The Norwegian lives in the first house
-    10 The man who plays baseball lives next to the man who keeps cats
-    11 The man who keeps horses lives next to the one who plays hockey
-    12 The man who plays billiards drinks beer
-    13 The German plays soccer
-    14 The Norwegian lives next to the blue house
-    15 The man who plays baseball has a neighbor who drinks water
+    A very simple print function to show the current solution .
+    
+    :param dict state: The state of the world.
     """
-    def test_assign_norweigen_to_first_house(self):
-        """
-        Assert rule 9 the norweigen lives in the first house.
-        """
-        # How do we represent this rule using a step from one state to the next
-        new_state = einstein.assign_value('1', 'nationality', 'norweigen', einstein.START_STATE)
-        self.assertEqual(new_state['1']['nationality'], set(['norweigen']))
-        self.assertNotIn('blue', new_state['2']['nationality'])
-        self.assertNotIn('blue', new_state['3']['nationality'])
-        self.assertNotIn('blue', new_state['4']['nationality'])
-        self.assertNotIn('blue', new_state['5']['nationality'])
+    def pad_values(state):
+        """ Specific padding for printing """
+        for house, properties in new_state.iteritems(): # In this house
+            for property, values in properties.iteritems(): # Examine the properties
+                for padder in ('....', '...', '..', '.'):
+                    if len(values) < 5:
+                        values.add(padder)
+                    
+    print '\n\n'
+    row = "{0:13s} | {1:13s}| {2:13s}| {3:13s}| {4:13s}| {5:13s}|"
+    spacer = "{0}|{0}|{0}|{0}|{0}|{0}|".format('--------------')
+    new_state = deepcopy(state)
+    pad_values(new_state)
+    
+    get_property = lambda ps: ps.pop()
+    
+    def print_row(title, property):
+        print row.format(
+            title,
+            get_property(new_state['1'][property]),
+            get_property(new_state['2'][property]),
+            get_property(new_state['3'][property]),
+            get_property(new_state['4'][property]),
+            get_property(new_state['5'][property])
+        )
+        
+    print spacer
+    for property in ('house_color', 'nationality', 'drink', 'sport', 'pet',):
+        print_row(property, property)
+        print_row('', property)
+        print_row('', property)
+        print_row('', property)
+        print_row('', property)
+        print spacer
 
-    def test_duplicate_assignment(self):
-        """
-        If we make the same assignment twice it won't cause a problem.
-        We will however like to see a message logged.
-        """
-        new_state = einstein.assign_value('1', 'nationality', 'norweigen', einstein.START_STATE)
-        self.assertEqual(new_state['1']['nationality'], set(['norweigen']))
-        self.assertNotIn('blue', new_state['2']['nationality'])
-        self.assertNotIn('blue', new_state['3']['nationality'])
-        self.assertNotIn('blue', new_state['4']['nationality'])
-        self.assertNotIn('blue', new_state['5']['nationality'])
-        new_state = einstein.assign_value('1', 'nationality', 'norweigen', new_state)
-        self.assertEqual(new_state['1']['nationality'], set(['norweigen']))
-        self.assertNotIn('blue', new_state['2']['nationality'])
-        self.assertNotIn('blue', new_state['3']['nationality'])
-        self.assertNotIn('blue', new_state['4']['nationality'])
-        self.assertNotIn('blue', new_state['5']['nationality'])
+def remove_value(position, property, value, state):
+    """
+    Remove a value from a houses property set.
 
-    def test_double_assignment(self):
-        """
-        If we attempt to assign the same value to two places we have gone
-        wrong and should raise an error.
-        """
-        new_state = einstein.assign_value('1', 'nationality', 'norweigen', einstein.START_STATE)
-        self.assertEqual(new_state['1']['nationality'], set(['norweigen']))
-        self.assertNotIn('blue', new_state['2']['nationality'])
-        self.assertNotIn('blue', new_state['3']['nationality'])
-        self.assertNotIn('blue', new_state['4']['nationality'])
-        self.assertNotIn('blue', new_state['5']['nationality'])
-        # We whould see state error on a double assignment
-        with self.assertRaises(einstein.StateError):
-            new_state = einstein.assign_value('2', 'nationality', 'norweigen', new_state)
+    :param str position: The key in the state dictionary identifying the house.
+    :param str property: The property in state[position] being updated.
+    :param str value: value is assigned to state[position][property].
+    :param dict state: The current state of the world.
+    :return dict: The new state of world.
+    """
+    new_state = deepcopy(state)
+    try:
+        new_state[position][property].remove(value)
+        logging.info('house %s, %s removed %s', position, property, value)
+    except KeyError:
+        #logging.warning("Value %s can't be removed from %s", value, property)
+        pass
+    finally:
+        if not new_state[position][property]:
+            raise StateError("House %s, %s no values left!", position, property)
+    return new_state
 
-    def test_bogus_assignment(self):
-        """
-        If we attemp to assign a bogus value we gat a KeyError
-        """
-        with self.assertRaises(KeyError):
-            einstein.assign_value('1', 'vehicle', 'car', einstein.START_STATE)
+def assign_value(position, property, value, state):
+    """
+    Updates a house's property to be a single value.  
+    Eliminates that value from the same property set in other houses.
+    
+     - The norweigen lives in the first house.
+    
+    Therefore he cannot live in any other houses.
 
-    def test_get_position(self):
-        """
-        If a value like 'norweigen' or 'dog' is the only value in a set of
-        potential values then its been assigned.
+    :param str position: The key in the state dictionary identifying the house.
+    :param str property: The property in state[position] being updated.
+    :param str value: value is assigned to state[position][property].
+    :param dict state: The current state of the world.
+    :return dict: The new state of world.
+    """
+    new_state = deepcopy(state)
+    logging.debug('house %s, %s = %s', position, property, value)
+    new_state[position][property] = set([value])
+    for house, details in new_state.iteritems():
+        if house != position:
+            new_state = remove_value(house, property, value, new_state)
+    return new_state
 
-        In this case return the house.
+def get_position(value, state):
+    """
+    Return the house where a specific value has been assigned to a property.
+    Assignment is true when that value is the only member of the set of possible
+    values for that property.
 
-        If there is no such case return None
-        """
-        new_state = deepcopy(einstein.START_STATE)
-        # Not assigned so it won't be found
-        self.assertIsNone(einstein.get_position('norweigen', new_state))
+    :param str value: The value you are searching for.
+    :param dict state: The current state of the universe.
+    :return str or None: The key representing the house or None.
+    """
+    for house, properties in state.iteritems():
+        for property, values in properties.iteritems():
+            if value in values and len(values) == 1:
+                return house
+    return None
 
-        # Assign it and we will find it
-        new_state = einstein.assign_value('1', 'nationality', 'norweigen', new_state)
-        self.assertEqual(new_state['1']['nationality'], set(['norweigen']))
-        self.assertEqual(einstein.get_position('norweigen', new_state), '1')
+def left_of(house):
+    """
+    Find the house to the left of this house.
+    
+    :param str house: the key of the house we are looking at.
+    """
+    left = str(int(house)-1)
+    return left if left in START_STATE.keys() else None
 
-        # It also won't feature anywhere else
-        self.assertNotIn('blue', new_state['2']['nationality'])
-        self.assertNotIn('blue', new_state['3']['nationality'])
-        self.assertNotIn('blue', new_state['4']['nationality'])
-        self.assertNotIn('blue', new_state['5']['nationality'])
+def right_of(house):
+    """
+    Find the house to the right of this house.
+    
+    :param str house: the key of the house we are looking at.
+    """
+    right = str(int(house)+1)
+    return right if right in START_STATE.keys() else None
 
-    def test_elimination_sweep(self):
-        """
-        If a set of values dwindles to just one value then the last_man_standing rule applies.
+def next_to(house):
+    """
+    Find the houses either side of this house.
+    
+    :param str house: the key of the house we are looking at.
+    """
+    return  [h for h in (left_of(house), right_of(house),) if h]
 
-        In this case officially assigning the value will correct state
-        """
-        new_state = einstein.deepcopy(einstein.START_STATE)
-        new_state['1']['nationality'] = set(['norweigen'])
-        new_state = einstein.elimination_sweep(new_state)
-        self.assertNotIn('blue', new_state['2']['nationality'])
-        self.assertNotIn('blue', new_state['3']['nationality'])
-        self.assertNotIn('blue', new_state['4']['nationality'])
-        self.assertNotIn('blue', new_state['5']['nationality'])
-        ### LOCK
-        self.assertEqual(einstein.get_position('norweigen', new_state), '1')
+def last_man_standing(state):
+    """
+    Generator to find assigned values. There are cases where several assignments
+    leave only one posibility for a property of a house. This is the last man 
+    standing rule. Essentially this is an implicit assignment.
 
-    def test_propose_link(self):
-        """
-        Proposing a value doesn't officially make an assignment.
+    :param dict state: The state of the universe.
+    :return dict or None: The house or None.
+    :yield tuple: (house, property, value)
+    """
+    for house, properties in state.iteritems():
+        for property, values in properties.iteritems():
+            if len(values) == 1:
+                yield (house, property, tuple(values)[0],)
 
-        If the Swede keeps a bird then no one else can.
-        If the Swede doesn't live in that house then neither can the bird.
-        """
-        # Proposing a value with no assignments made won't change state
-        new_state = deepcopy(einstein.START_STATE)
-        new_state = einstein.propose_link('nationality', 'swedish', 'pet', 'bird', new_state)
-        self.assertEqual(new_state, einstein.START_STATE)
+def elimination_sweep(state):
+    """
+    Traverse the state and assert that anything discovered by the last man 
+    standing rule is correctly assigned.
+    
+    :param dict state: The state of the universe.
+    """
+    new_state = deepcopy(state)
+    for house, property, value in last_man_standing(state):
+        new_state = assign_value(house, property, value, new_state)
+    return new_state
 
-        # The norweigen cannot own a bird, because the swede does.
-        new_state = einstein.assign_value('1', 'nationality', 'norweigen', new_state)
-        new_state = einstein.propose_link('nationality', 'swedish', 'pet', 'bird', new_state)
-        self.assertNotIn('bird', new_state['1']['pet'])
+def propose_link(assignee_property, assignee_value, assignment_property, assignment_value, state):
+    """
+    Only the assignee can have this assignment.
 
-        # If house 3 owns a horse then the swede cannot live there, because he owns a bird.
-        new_state = einstein.assign_value('3', 'pet', 'horse', new_state)
-        new_state = einstein.propose_link('nationality', 'swedish', 'pet', 'bird', new_state)
-        self.assertNotIn('swedish', new_state['3']['nationality'])
+    If a Swede keeps a bird then no one else can.
 
-    def test_next_to_houses(self):
-        """
-        Some rules tell you that next to a house a certain condition is true.
+    :param assignee_property: The owner property eg 'nationality'
+    :param assignee_value: The owner value eg 'Swede'
+    :param assignment_property: The assignment property eg 'pet'
+    :param assignment_value: The assignment value eg 'bird'
+    :param dict state: The state of the universe.
+    """
+    new_state = deepcopy(state)
+    for house, properties in new_state.iteritems(): # In this house
+        for property, values in properties.iteritems(): # Examine the properties
 
-        The white house is to the left of the green house.
-        House 2 is to the right of house 1.
-        1, 2, 3, 4, 5
-        """
-        self.assertIsNone(einstein.left_of('1'))
-        self.assertIsNone(einstein.right_of('5'))
-        self.assertIsNone(einstein.left_of('20'))
-        self.assertEqual('1', einstein.left_of('2'))
-        self.assertEqual('3', einstein.left_of('4'))
-        self.assertEqual('3', einstein.right_of('2'))
-        self.assertEqual(['2', '4'], einstein.next_to('3'))
-        self.assertEqual(['2'], einstein.next_to('1'))
-        self.assertEqual(['4'], einstein.next_to('5'))
+            if assignee_property == property: # If the property nationality
+                if assignee_value not in values: # But its not the Swede
+                    # Then remove the bird from the set of pets in this house.
+                    new_state = remove_value(house, assignment_property, assignment_value, new_state)
+
+            if assignment_property == property: # If the property is the pet
+                if len(values) == 1: # and only one pet is set
+                    if assignment_value not in values: # and the pet is not a bird
+                        # Then the Swede cannot live in this house.
+                        new_state = remove_value(house, assignee_property, assignee_value, new_state)
+    return new_state
+
+def propose_house(positions, property, value, state):
+    """
+    Proposing a specific house has a property means no other house can have it.
+    
+     - The house nest to the green house is white.
+    
+    This means we should ensure white remains in the house_color set for any
+    house nest to the green one.
+    And ensure white is removed from all other house_color sets.
+
+    :param list positions: Keys in the state dictionary identifying the house.
+    :param str property: The property in state[position] being updated.
+    :param str value: value is assigned to state[position][property].
+    :param dict state: The current state of the world.
+    :return dict: The new state of world.
+    """
+    new_state = deepcopy(state)
+    for house, properties in new_state.iteritems(): # In this house
+        if house not in positions:
+            new_state = remove_value(house, property, value, new_state)
+    return new_state
+
+def end_solution(state):
+    """
+    If every property set has only 1 value left then we have solved the puzzle.
+    
+    :param dict state: The current state of the world.
+    """
+    for house, properties in state.iteritems(): # In this house
+        for property, values in properties.iteritems(): # Examine the properties
+            if len(values) > 1:
+                return False
+    return True
