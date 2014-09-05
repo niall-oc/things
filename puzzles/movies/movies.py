150089debc434471a00662dd31c9f50fa2a14ee8
Niall O'Connor <zechs.marquie@gmail.com>
2014-09-05 16:28:22 +0100

    Add main to movies and a sample intpu file

--- puzzles/movies/movies.py
+++ puzzles/movies/movies.py
@@ -1,6 +1,28 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+"""
+Given the following list in a string seperated by \n characters.
+    Jaws (1975)
+    Starwars 1977
+    2001 A Space Odyssey ( 1968 )
+    Back to the future 1985.
+    Raiders of the lost ark 1981 .
+    jurassic park 1993
+    The Matrix 1999
+    A fist full of Dollars
+    10,000 BC (2008)
+    1941 (1979)
+    24 Hour Party People (2002)
+    300 (2007)
+    2010
 
+Produce the following output.
+    2000s : 3
+    1970s : 3
+    1980s : 2
+    1990s : 2
+    1960s : 1
+"""
 import re
 
 year_pattern = re.compile("\d{4}") # or [0-9]{4}
@@ -61,4 +83,11 @@ def rank_decades(movies):
             decade = "{0}0s".format(year[:3])
             results[decade] = results.setdefault(decade, 0) + 1
     return results
-    
+
+if __name__ == "__main__":
+    f = open('movie_releases.txt')
+    movie_data = f.read()
+    all_movies = movie_data.split('\n')
+    rank = rank_decades(all_movies)
+    for decade, count in sorted(rank.items(), key=lambda s: s[1], reverse=True):
+        print "%s : %s" % (decade, count,)
