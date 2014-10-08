911cce8c471651611cbd07adb9ed2c177f942254
Niall O'Connor <git@heroku.com:moften.git>
2014-10-08 22:42:46 +0100

    First test to find neighbours

---
+++ puzzles/gameoflife/game_of_life.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Conways Game of Life
+
+Rules
+
+# Any live cell with fewer than two live neighbours dies.
+                        
+    #      -->          
+                        
+# Any live cell with two or three live neighbours remains alive.
+                        
+    ##     -->     ##   
+    #              #    
+
+# Any live cell with more than three live neighbours dies.
+                        
+    #              #    
+    ##     -->          
+    #              #    
+
+# Any dead cell with exactly three live neighbours becomes a live cell.
+                        
+    #                   
+    #      -->    ###   
+    #                   
+"""
+
+def find_neighbours(cell, grid):
+    """
+    A neighbour is any living cell that is in within 1x or 1y of the current
+    cell.  Consider the folowing
+    
+    (1, 1)  (2, 1)  (3, 1)
+    (1,-1)  (2,-1)  (3,-1)  
+    
+    >>> find_neighbours((1,1))
+    set([(1,-1), (2, 1), (2,-1)])
+    
+    :param tuple cell: The x,y position of the cell.
+    :param set grid: The state of the universe.
+    :return set: The neighbours of cell.
+    """
+    x, y = cell
+    potential = set([(xp, yp) for xp in (x-1, x, x+1) for yp in (y-1, y, y+1)])
+    # The cell itself is not a neighbour
+    potential.remove(cell)
+    return potential.intersection(grid)
+    
+    
+    
+    
---
+++ puzzles/gameoflife/test_game_of_life.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Conways Game of Life
+
+Rules
+
+# Any live cell with fewer than two live neighbours dies.
+                        
+    #      -->          
+                        
+# Any live cell with two or three live neighbours remains alive.
+                        
+    ##     -->     ##   
+    #              #    
+
+# Any live cell with more than three live neighbours dies.
+                        
+    #              #    
+    ##     -->          
+    #              #    
+
+# Any dead cell with exactly three live neighbours becomes a live cell.
+                        
+    #                   
+    #      -->    ###   
+    #                   
+"""
+import unittest
+import game_of_life
+
+class GameOfLife(unittest.TestCase):
+    def test_find_neighbours(self):
+        """
+        A neighbour is any living cell that is in within 1x or 1y of the current
+        cell.  Consider the folowing
+        
+        (1, 1)  (2, 1)  (3, 1)
+        (1,-1)  (2,-1)  (3,-1)  
+        
+        >>> find_neighbours((1,1))
+        set([(1,-1), (2, 1), (2,-1)])
+        """
+        grid = set([(1,1), (2,2), (3,3)])
+        neighbours = game_of_life.find_neighbours((1,1), grid)
+        self.assertIn((2,2), neighbours)
+        self.assertNotIn((3,3), neighbours)
+        neighbours = game_of_life.find_neighbours((2,2), grid)
+        self.assertNotIn((2,2), neighbours)
+        self.assertIn((3,3), neighbours)
+        self.assertIn((1,1), neighbours)
+        
+        
