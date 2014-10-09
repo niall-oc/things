c20f0ce5cab22f96d55b565bc29bc528a6e7a60c
Niall O'Connor <git@heroku.com:moften.git>
2014-10-10 00:12:49 +0100

    Engine ready

--- puzzles/gameoflife/game_of_life.py
+++ puzzles/gameoflife/game_of_life.py
@@ -27,26 +27,56 @@ Rules
     #                   
 """
 
+def find_surrounding(cell):
+    """
+    A surrounding is any space that is in within 1x or 1y of the current
+    cell.
+    
+    >>> find_surrounding((0,0))
+    set([(-1,1), (0,1), (1,1), (-1,0), (1,0), (-1,-1), (0,-1), (1,-1)])
+    
+    :param tuple cell: The x,y position of the cell.
+    :return set: The surrounding spaces
+    """
+    x, y = cell
+    surrounding = set([(xp, yp) for xp in (x-1, x, x+1) for yp in (y-1, y, y+1)])
+    # The cell itself is not a surrounding space
+    surrounding.remove(cell)
+    return surrounding
+
 def find_neighbours(cell, grid):
     """
     A neighbour is any living cell that is in within 1x or 1y of the current
     cell.  Consider the folowing
     
-    (1, 1)  (2, 1)  (3, 1)
-    (1,-1)  (2,-1)  (3,-1)  
+    (1,1)  (2,1)  (3,1)
+    (1,0)  (2,0)  (3,0)  
     
     >>> find_neighbours((1,1))
-    set([(1,-1), (2, 1), (2,-1)])
+    set([(1,0), (2,0), (2,1)])
     
     :param tuple cell: The x,y position of the cell.
     :param set grid: The state of the universe.
     :return set: The neighbours of cell.
     """
-    x, y = cell
-    potential = set([(xp, yp) for xp in (x-1, x, x+1) for yp in (y-1, y, y+1)])
-    # The cell itself is not a neighbour
-    potential.remove(cell)
+    potential = find_surrounding(cell)
     return potential.intersection(grid)
+
+def find_spaces_around_cells(grid):
+    """
+    For every cell in the grid find the surrounding spaces.
+    
+    :param set grid: The state of the universe.
+    :return set: The set of spaces surrounding cells.
+    """
+    spaces = set()
+    for cell in grid:
+        # Find all the cells around this one
+        surrounding = find_surrounding(cell)
+        # Determine which of the surrounding spaces have no cells
+        # Add those spaces to the spaces set
+        spaces = spaces.union(surrounding.difference(grid))
+    return spaces
 
 def step(grid):
     """
@@ -59,10 +89,16 @@ def step(grid):
     """
     # rule 1.
     new_grid = set()
+    # For each cell in the grid only allow those with 2 or 3 neoghbours to live.
     for cell in grid:
         neighbours = find_neighbours(cell, grid)
-        len_neighbours = len(neighbours)
-        if len_neighbours in (2,3,):
+        if len(neighbours) in (2,3,):
             new_grid.add(cell)
-        
+    
+    # For each space around a cell only allow those with 3 neighbours to live.
+    for space in find_spaces_around_cells(grid):
+        neighbours = find_neighbours(space, grid)
+        if len(neighbours) == 3:
+            new_grid.add(space)
+    
     return new_grid
--- puzzles/gameoflife/test_game_of_life.py
+++ puzzles/gameoflife/test_game_of_life.py
@@ -30,6 +30,18 @@ import unittest
 import game_of_life
 
 class GameOfLife(unittest.TestCase):
+    def test_find_surrounding(self):
+        """
+        A surrounding is any space that is in within 1x or 1y of the current
+        cell.
+        
+        >>> game_of_life.find_surrounding((0,0))
+        set([(-1,1), (0,1), (1,1), (-1,0), (1,0), (-1,-1), (0,-1), (1,-1)])
+        """
+        new_grid = game_of_life.find_surrounding((0,0))
+        expected = set([(-1,1), (0,1), (1,1), (-1,0), (1,0), (-1,-1), (0,-1), (1,-1)])
+        self.assertEqual(new_grid, expected)
+        
     def test_find_neighbours(self):
         """
         A neighbour is any living cell that is in within 1x or 1y of the current
@@ -49,6 +61,23 @@ class GameOfLife(unittest.TestCase):
         self.assertNotIn((2,2), neighbours)
         self.assertIn((3,3), neighbours)
         self.assertIn((1,1), neighbours)
+    
+    def test_find_spaces_around_cells(self):
+        """
+        Find the spaces around a cell that are empty.
+        
+        >>> grid = set([(0,0), (1,0)])
+        >>> game_of_life.find_surrounding(grid)
+        set([(-1, 1), (0, 1), (1, 1), (2, 1), 
+             (-1, 0),                 (2, 0), 
+             (-1,-1), (0,-1), (1,-1), (2,-1)])
+        """
+        grid = set([(0,0), (1,0)])
+        expected = set([(-1, 1), (0, 1), (1, 1), (2, 1), 
+                        (-1, 0),                 (2, 0), 
+                        (-1,-1), (0,-1), (1,-1), (2,-1)])
+        new_grid = game_of_life.find_spaces_around_cells(grid)
+        self.assertEqual(new_grid, expected)
         
     def test_rule_one(self):
         """
@@ -66,11 +95,12 @@ class GameOfLife(unittest.TestCase):
         # Any live cell with two or three live neighbours remains alive.
                                 
             ##     -->     ##   
-            #              #    
+             #             ##   
         """
         grid = set([(1,0), (2,0), (2,-1)])
+        expected = set([(1,0), (2,0), (1,-1), (2,-1)])
         new_grid = game_of_life.step(grid)
-        self.assertEqual(grid, new_grid)
+        self.assertEqual(expected, new_grid)
         
     def test_rule_three(self):
         """
@@ -83,3 +113,19 @@ class GameOfLife(unittest.TestCase):
         grid = set([(1,0), (2,0), (3,0), (2,1), (2,-1)])
         new_grid = game_of_life.step(grid)
         self.assertNotIn((2,0), new_grid)
+
+    def test_rule_four(self):
+        """
+        # Any dead cell with exactly three live neighbours becomes a live cell.
+                        
+            #                   
+            #      -->    ###   
+            #                   
+        """
+        grid = set([(1,0), (2,0), (3,0)])
+        new_grid = game_of_life.step(grid)
+        self.assertNotIn((1,0), new_grid)
+        self.assertNotIn((3,0), new_grid)
+        self.assertIn((2,0), new_grid)
+        self.assertIn((2,1), new_grid)
+        self.assertIn((2,-1), new_grid)
