3e3142807f07edf17826e93924a3f55a8eb5bafe
niall-oc <zechs.marquie@gmail.com>
2015-11-24 15:31:21 +0000

    First draft of test

---
+++ tec/bdd/test_square.py
@@ -0,0 +1,21 @@
+from unittest2 import TestCase
+import square
+
+
+class SquareTest(TestCase):
+
+    def setup(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_build_square_successfully(self):
+        """
+        Given
+            1. x,y coord representing the top left of a square.
+            2, Length of one edge.
+        return
+            ???
+        """
+        
