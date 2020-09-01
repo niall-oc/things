04903d6dca4f47371cff8ba0091078a6cd8398b2
Niall O'Connor <zechs.marquie@gmail.com>
2020-09-01 16:40:40 +0100

    Update solutions.py
    Max counters 100% solution.

--- codility/solutions.py
+++ codility/solutions.py
@@ -84,6 +84,23 @@ def solution(A):
             
     return -1
 
+# https://app.codility.com/programmers/lessons/4-counting_elements/max_counters/
+def solution(N, A):
+    count = [0] * N
+    max_count = 0
+    last_max = False
+    for val in A:
+        if val == N + 1 and last_max == False:
+            count = [max_count] * N
+            last_max = True
+            continue
+        if val <= N:
+            count[val - 1] += 1
+            max_count = max(count[val - 1], max_count)
+            last_max = False
+    return count
+
+
 # https://app.codility.com/programmers/lessons/6-sorting/max_product_of_three/
 def solution(A):
     r = max(A)
