ed1cc4c0ea8e4dcff7a10a4a5ac101e2441e3b03
Niall O'Connor <zechs.marquie@gmail.com>
2020-09-21 18:41:19 +0100

    Update solutions.py
    Average min of slices of array

--- codility/solutions.py
+++ codility/solutions.py
@@ -251,3 +251,25 @@ def solution(S, P, Q):
             results.append(4) # weight of a T
         idx += 1
     return results
+
+#https://app.codility.com/programmers/lessons/5-prefix_sums/min_avg_two_slice/
+def solution(A):
+    # write your code in Python 3.6
+    prefix_sum = [0] * len(A)
+    avg = (A[0] + A[1]) / 2
+    min_idx = 0
+    
+    idx = 2
+    while idx < len(A):
+        cur = (A[idx-2] + A[idx-1] + A[idx]) / 3
+        if cur < avg :
+            avg = cur
+            min_idx = idx-2
+        
+        cur = (A[idx-1] + A[idx]) / 2
+        if cur < avg :
+            avg = cur
+            min_idx = idx-1
+        idx += 1
+    
+    return min_idx
