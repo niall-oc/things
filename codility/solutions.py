291610eb8db1baf8d40394d07177b55ff303fa43
Niall O'Connor <zechs.marquie@gmail.com>
2020-09-24 18:36:42 +0100

    Update solutions.py
    Find the leader where x occurs in A more than len(A)//2 times

--- codility/solutions.py
+++ codility/solutions.py
@@ -306,3 +306,20 @@ def solution(A):
                 return 1
             i+=1
     return 0
+
+# https://app.codility.com/programmers/lessons/8-leader/dominator/
+def solution(A):
+    # write your code in Python 3.6
+    candidate = []
+    index = []
+    for i in range(len(A)):
+        if not candidate or A[i] == candidate[-1]:
+            candidate.append(A[i])
+            index.append(i)
+        else:
+            candidate.pop()
+            index.pop()
+    if candidate:
+        count = sum([1 for i in A if i == candidate[0]])
+        return index[0] if count > len(A) // 2 else -1
+    return -1
