201b127bf58c385d045ef37f2325166717c4f52a
Niall O'Connor <zechs.marquie@gmail.com>
2020-09-24 17:30:08 +0100

    Update solutions.py
    Find triangle

--- codility/solutions.py
+++ codility/solutions.py
@@ -293,3 +293,16 @@ def solution(A):
         i+=1
     
     return passes
+
+# https://app.codility.com/programmers/lessons/6-sorting/triangle/
+def solution(A):
+    # write your code in Python 3.6
+    A.sort()
+    N = len(A)
+    i = 2
+    if N > i:
+        while i < N:
+            if A[i-2] + A[i-1] > A[i]:
+                return 1
+            i+=1
+    return 0
