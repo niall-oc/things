ca8107b9602275233ffb1e7c4d7c10fcc2944c2d
Niall O'Connor <zechs.marquie@gmail.com>
2020-09-28 14:16:02 +0100

    Update solutions.py
    Max Slice sum requires handling negatives

--- codility/solutions.py
+++ codility/solutions.py
@@ -397,3 +397,13 @@ def solution(A):
     for i in range(1, n):
         moves[i] = A[i] - A[i-1]
     return max_slice_prof(moves, 0, n)
+
+#https://app.codility.com/programmers/lessons/9-maximum_slice_problem/max_slice_sum/
+def solution(A):
+    base = min(A)
+    ending = 0
+    max_slice = base
+    for a in A:
+        ending = max(base, ending + a, a)
+        max_slice = max(max_slice, ending)
+    return max_slice
