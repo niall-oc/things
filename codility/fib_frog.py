9f380c9bfc1c4360f7d135b5ca30eb3103d022ce
niall-oc <zechs.marquie@gmail.com>
2020-10-27 16:04:47 +0000

    100% solution found for fib_frog.

--- codility/fib_frog.py
+++ codility/fib_frog.py
@@ -11,6 +11,11 @@ def gen_fib(n):
     return fn
 
 def new_paths(A, n, last_pos, fn):
+    """
+    Given an array A of len n.
+    From index last_pos which numbers in fn jump to a leaf?
+    returns list: set of indexes with leaves.
+    """
     paths = []
     for f in fn:
         new_pos = last_pos + f
@@ -21,30 +26,34 @@ def new_paths(A, n, last_pos, fn):
 
 def solution(A):
     n = len(A)
-    #print(n, list(enumerate(A)))
     if n < 3:
         return 1
 
-    fn = sorted(gen_fib(100000)[2:])
-    #print(fn)
-    # a queue of paths that are still solvable!
-    paths = {-1: 0}
-    iteration = 1
+    # A.append(1) # mark final jump
+    fn = sorted(gen_fib(100000)[2:]) # Fib numbers with 0, 1, 1, 2..  clipped to just 1, 2..
+    # print(fn)
+    paths = {0:set([-1])} # locate all the leaves that are one fib jump from the start position.
+    result = 0
+
+    jump = 1
     while True:
-        #print(paths)
-        cp = []
-        for lp, it in paths.items():
-            np = new_paths(A, n, lp, fn)
-            #print(iteration, lp, np)
-            cp.extend(np)
-        if not cp:
+        # Considering each of the previous jump positions - How many leaves from there are one fib jump away
+        new_jumps =  set([idx for pos in paths[jump-1] for idx in new_paths(A, n, pos, fn)])
+
+        # no new jumps means game over!
+        if not new_jumps:
             break
-        paths = {p: min(iteration, paths.get(p, 10000000)) for p in cp}
-        #print(paths)
-        iteration += 1
-    if n in paths:
-        return paths[n]
+        # else update the paths
+        paths[jump] = new_jumps
+
+        # If there was a result in the new jumps record that
+        if n in new_jumps:
+            return jump
+        jump += 1
+
     return -1
+
+
 
 if __name__ == '__main__':
     tests = (
