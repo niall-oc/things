b4576e49983e7fb20c69503638c1151665ecf145
niall-oc <zechs.marquie@gmail.com>
2020-11-04 17:05:59 +0000

    Was 100% stashing this copy to figure out the 100%

--- codility/peaks.py
+++ codility/peaks.py
@@ -1,5 +1,7 @@
 # https://app.codility.com/programmers/lessons/10-prime_and_composite_numbers/peaks/
 # 72% solution
+import time
+
 def get_chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
@@ -9,7 +11,7 @@ def solution(A):
     # determine N the lenght of the array
     N = len(A)
     if N<3:
-        return 0
+        return 0 # no peaks in this case
     
     # scan the array to count the increasing slope of peaks.
     scan = [0]* N
@@ -20,33 +22,41 @@ def solution(A):
             scan[i] = scan[i-1]
     scan[-1] = scan[-2] # tag last
     
-    if scan[0] == scan[-1]: #no peaks
+    if scan[0] == scan[-1]: # if no peaks occur then exit
         return 0
     
     # determine the divisors of len(A)
     h_div = int(N**.5)
-    divisors = set()
-    while h_div > 1:  # one block is a given and N blocks cannot all contain peaks
-        if not N%h_div:
-            divisors.add(h_div)
-            divisors.add(N//h_div)
-        h_div -= 1
-    divisors.add(N)
-    divisors = sorted(list(divisors), reverse=True)
+
+    # The union of all divisors is created and sorted. Starting with the smallest divisor 
+    # divides into the max even parts first in order to find a solution quickest.
+    divisors = sorted({N} | { i for i in range(h_div, 1, -1) if not N%h_div } | { N//i for i in range(h_div, 1, -1) if not N%h_div })
 
     # print(N, divisors, scan)
-    i = 0
-    result = 0
-    while i < len(divisors):
-
-        chunks = [c[-1]-c[0] for c in get_chunks(scan, divisors[i])]
-        # print(chunks)
-        if all(chunks): # if a chunk dont have a peak then break the loop
-            result = len(chunks)
-        else:
-            break
-        i+=1
-    return result
+    for d in divisors: # Begin the search
+        # get the start and end peak count from each chunk
+        chunks = tuple(get_chunks(scan, d))
+        result = len(chunks)
+        # print(result, chunks)
+        # for all chunks if either a peak occurs within the cunk OR the begining of the chunk is a peak!
+        peaks = [(chunks[i][-1]-chunks[i][0]) or chunks[i][0] > chunks[max(i-1,0)][-1] for i in range(result)]
+        
+        if all(peaks): # if every chunk has a peak and this is the most chunks we can break the Array into!
+            # print(chunks, peaks)
+            return result
+    return 0
 
 if __name__ == '__main__':
-    print(solution([1,3,2,1]))
+    tests = (
+        (1, ([0,1,0,1,0],)),
+        (2, ([0,1,0,1,0,0],)),
+    )
+    for expected, args in tests:
+        tic = time.perf_counter()
+        res = solution(*args)
+        toc = time.perf_counter()
+        print(f'ARGS produced {res} in {toc - tic:0.8f} seconds')
+        try:
+            assert(expected == res)
+        except AssertionError as e:
+            print(f'ERROR {args} produced {res} when {expected} was expected!')
