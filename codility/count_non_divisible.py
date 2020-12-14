1bcbb426b599171eb0a8a17befe583f97b6f7cf7
niall-oc <zechs.marquie@gmail.com>
2020-12-14 17:50:43 +0000

    100% solutions for lesson 11

--- codility/count_non_divisible.py
+++ codility/count_non_divisible.py
@@ -1,4 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Author: Niall O'Connor
+
 # https://app.codility.com/programmers/lessons/11-sieve_of_eratosthenes/count_non_divisible/
+
+
+You are given an array A consisting of N integers.
+
+For each number A[i] such that 0 ≤ i < N, we want to count the number of elements 
+of the array that are not the divisors of A[i]. We say that these elements are 
+non-divisors.
+
+For example, consider integer N = 5 and array A such that:
+    A[0] = 3
+    A[1] = 1
+    A[2] = 2
+    A[3] = 3
+    A[4] = 6
+
+For the following elements:
+
+        A[0] = 3, the non-divisors are: 2, 6,
+        A[1] = 1, the non-divisors are: 3, 2, 3, 6,
+        A[2] = 2, the non-divisors are: 3, 3, 6,
+        A[3] = 3, the non-divisors are: 2, 6,
+        A[4] = 6, there aren't any non-divisors.
+
+Write a function:
+
+    def solution(A)
+
+that, given an array A consisting of N integers, returns a sequence of integers 
+epresenting the amount of non-divisors.
+
+Result array should be returned as an array of integers.
+
+For example, given:
+    A[0] = 3
+    A[1] = 1
+    A[2] = 2
+    A[3] = 3
+    A[4] = 6
+
+the function should return [2, 4, 3, 2, 0], as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+        N is an integer within the range [1..50,000];
+        each element of array A is an integer within the range [1..2 * N].
+
+# 100% solution https://app.codility.com/demo/results/trainingH93PHF-566/ O(n*log(n))
+"""
+
+import time
 
 def get_divisors(n):
     froot = int(n**.5)
@@ -12,21 +66,44 @@ def get_divisors(n):
     return divs
 
 def solution(A):
+    """
+    1. scan the array and record the frequency of all integers.
+    2. for each divisor find its frequency in the array.
+    3. Non Divisors = N - The total occurence of all divisors
+    """
     N = len(A)
     int_count = {}
     
     # O(N) scan to count number frequency
-    for i in range(N):
-        int_count[A[i]] = int_count.get(A[i], 0) + 1
+    for i in A:
+        int_count[i] = int_count.get(i, 0) + 1
     
     # Create an array for every i's non-divisor count
-    div_count = {}
+    non_div_count = {}
     
-    for i, freq in int_count.items():
+    for i, _ in int_count.items(): # only calculate numbers once
         divs = get_divisors(i)
-        num_divs = 0
-        for d in divs:
-            num_divs += int_count.get(d, 0)
-        div_count[i] = N-num_divs # N -  divisors = non-divisors :-)
+        # non-divisors = N -  divisors :-)
+        non_div_count[i] = N - sum([int_count.get(d, 0) for d in divs])
         
-    return [div_count[A[i]] for i in range(N)]
+    return [non_div_count[i] for i in A]
+
+if __name__ == '__main__':
+    tests = (
+        ([2, 4, 3, 2, 0], ([3, 1, 2, 3, 6],)),
+        ([0], ([2],)),
+        #(None, (range(30000),)),
+    )
+    for expected, args in tests:
+        tic = time.perf_counter()
+        res = solution(*args)
+        toc = time.perf_counter()
+        
+        if expected is None:
+            print(f'SPEED-TEST {len(args[0])} args finished in {toc - tic:0.8f} seconds')
+            continue # This is just a speed test
+        print(f'ARGS produced {res} in {toc - tic:0.8f} seconds')
+        try:
+            assert(expected == res)
+        except AssertionError as e:
+            print(f'ERROR {args} produced {res} when {expected} was expected!')
--- codility/count_semiprimes.py
+++ codility/count_semiprimes.py
@@ -1,5 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Author: Niall O'Connor
+
 # https://app.codility.com/programmers/lessons/11-sieve_of_eratosthenes/count_semiprimes/
-# 77% solution 2 perf tests very close
+
+A prime is a positive integer X that has exactly two distinct divisors: 1 and X. 
+The first few prime integers are 2, 3, 5, 7, 11 and 13.
+
+A semiprime is a natural number that is the product of two (not necessarily distinct) 
+prime numbers. The first few semiprimes are 4, 6, 9, 10, 14, 15, 21, 22, 25, 26.
+
+You are given two non-empty arrays P and Q, each consisting of M integers. These 
+arrays represent queries about the number of semiprimes within specified ranges.
+
+Query K requires you to find the number of semiprimes within the range (P[K], Q[K]), 
+where 1 ≤ P[K] ≤ Q[K] ≤ N.
+
+For example, consider an integer N = 26 and arrays P, Q such that:
+    P[0] = 1    Q[0] = 26
+    P[1] = 4    Q[1] = 10
+    P[2] = 16   Q[2] = 20
+
+The number of semiprimes within each of these ranges is as follows:
+
+        (1, 26) is 10,
+        (4, 10) is 4,
+        (16, 20) is 0.
+
+Write a function:
+
+    def solution(N, P, Q)
+
+that, given an integer N and two non-empty arrays P and Q consisting of M integers, 
+returns an array consisting of M elements specifying the consecutive answers to 
+all the queries.
+
+For example, given an integer N = 26 and arrays P, Q such that:
+    P[0] = 1    Q[0] = 26
+    P[1] = 4    Q[1] = 10
+    P[2] = 16   Q[2] = 20
+
+the function should return the values [10, 4, 0], as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+        N is an integer within the range [1..50,000];
+        M is an integer within the range [1..30,000];
+        each element of arrays P, Q is an integer within the range [1..N];
+        P[i] ≤ Q[i].
+
+# 100% solution https://app.codility.com/demo/results/trainingF5KA5F-PVP/ 
+O(N * log(log(N)) + M) 
+"""
+
+import time
 
 def get_sieve(n):
     # Use the sieve or eratosthenes to produce an array of primes
@@ -26,21 +80,32 @@ def is_semi_prime(n, factors):
     return False
 
 def solution(N, P, Q):
-    # produce a slope of increasing semi primes
+    """
+    1. produce a slope of increasing semi primes from 0 to N
+    2. measure the increase from slope[Q[i]] to slope[P[i]]
+    """
     factors = get_sieve(N)
     slope = [0] * (N+1)
     for i in range(1, N+1):
         slope[i] = slope[i-1] + is_semi_prime(i, factors) # Auto casting!! :-)
     # Optimus Prime!
-    print(list(enumerate(slope)))
+    # print(list(enumerate(slope)))
     return [slope[Q[j]] - slope[P[j]-1] for j in range(len(P))]
 
-
 if __name__ == '__main__':
-    test_cases = (
+    tests = (
         ( [10,4,0, 10, 11], (33, [1, 4, 16, 4, 1], [26, 10, 20, 28, 33]), ),
     )
-    for expected, args in test_cases:
-        got = solution(*args)
-        print(expected, got)
-        assert(expected == got)
+    for expected, args in tests:
+        tic = time.perf_counter()
+        res = solution(*args)
+        toc = time.perf_counter()
+        
+        if expected is None:
+            print(f'SPEED-TEST {len(args[0])} args finished in {toc - tic:0.8f} seconds')
+            continue # This is just a speed test
+        print(f'ARGS produced {res} in {toc - tic:0.8f} seconds')
+        try:
+            assert(expected == res)
+        except AssertionError as e:
+            print(f'ERROR {args} produced {res} when {expected} was expected!')
