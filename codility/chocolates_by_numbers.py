10dc0ad03ad3431e208718a8d6ebfedd34f547ef
Niall <zechs.marquie@gmail.com>
2020-12-21 22:25:04 +0000

    Added links and 100% solutions for lesson 12

--- codility/chocolates_by_numbers.py
+++ codility/chocolates_by_numbers.py
@@ -1,6 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Author: Niall O'Connor
+
 # https://app.codility.com/programmers/lessons/12-euclidean_algorithm/chocolates_by_numbers/
 
+Two positive integers N and M are given. Integer N represents the number
+of chocolates arranged in a circle, numbered from 0 to N − 1.
+
+You start to eat the chocolates. After eating a chocolate you leave only
+a wrapper.
+
+You begin with eating chocolate number 0. Then you omit the next M − 1
+chocolates or wrappers on the circle, and eat the following one.
+
+More precisely, if you ate chocolate number X, then you will next eat the
+chocolate with number (X + M) modulo N (remainder of division).
+
+You stop eating when you encounter an empty wrapper.
+
+For example, given integers N = 10 and M = 4. You will eat the following
+chocolates: 0, 4, 8, 2, 6.
+
+The goal is to count the number of chocolates that you will eat,
+following the above rules.
+
+Write a function:
+
+def solution(N, M)
+
+that, given two positive integers N and M, returns the number of chocolates
+that you will eat.
+
+For example, given integers N = 10 and M = 4. the function should return 5,
+as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+N and M are integers within the range [1..1,000,000,000].
+
+# 100% solution https://app.codility.com/demo/results/trainingTQJ2KT-TEU/
+O(log(N + M))
+"""
+
+import time
+
 def division_sol(n, m):
+    """
+    The division solution has similar complexity but fails
+    to pass the performance test for extreme large cases.
+    Its worth noting that this solution cannot blow the recursion stack.
+    """
     mod = n%m
     division = n//m
     start = end = m
@@ -20,3 +69,21 @@ def gcd_division(a, b):
 
 def solution(n, m):
     return n // gcd_division(n, m)
+
+if __name__ == '__main__':
+    tests = (
+        ( 5, (10,4,)),
+    )
+    for expected, args in tests:
+        tic = time.perf_counter()
+        res = solution(*args)
+        toc = time.perf_counter()
+        
+        if expected is None:
+            print(f'SPEED-TEST {len(args[0])} args finished in {toc - tic:0.8f} seconds')
+            continue # This is just a speed test
+        print(f'ARGS produced {res} in {toc - tic:0.8f} seconds')
+        try:
+            assert(expected == res)
+        except AssertionError as e:
+            print(f'ERROR {args} produced {res} when {expected} was expected!')
--- codility/common_prime_divisors.py
+++ codility/common_prime_divisors.py
@@ -1,4 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Author: Niall O'Connor
+
 # https://app.codility.com/programmers/lessons/12-euclidean_algorithm/common_prime_divisors/
+
+A prime is a positive integer X that has exactly two distinct divisors:
+1 and X. The first few prime integers are 2, 3, 5, 7, 11 and 13.
+
+A prime D is called a prime divisor of a positive integer P if there
+exists a positive integer K such that D * K = P. For example, 2 and
+5 are prime divisors of 20.
+
+You are given two positive integers N and M. The goal is to check
+whether the sets of prime divisors of integers N and M are exactly
+the same.
+
+For example, given:
+
+    N = 15 and M = 75, the prime divisors are the same: {3, 5};
+    N = 10 and M = 30, the prime divisors aren't the same: {2, 5} is
+    not equal to {2, 3, 5};
+    N = 9 and M = 5, the prime divisors aren't the same: {3} is not
+    equal to {5}.
+Write a function:
+
+def solution(A, B)
+
+that, given two non-empty arrays A and B of Z integers, returns the
+number of positions K for which the prime divisors of A[K] and B[K]
+are exactly the same.
+
+For example, given:
+
+    A[0] = 15   B[0] = 75
+    A[1] = 10   B[1] = 30
+    A[2] = 3    B[2] = 5
+the function should return 1, because only one pair (15, 75) has the
+same set of prime divisors.
+
+Write an efficient algorithm for the following assumptions:
+
+Z is an integer within the range [1..6,000];
+each element of arrays A, B is an integer within the range
+[1..2,147,483,647].
+
+# 100% solution https://app.codility.com/demo/results/trainingYZA3X2-JA4/
+O(Z * log(max(A) + max(B))**2)
+"""
+
+import time
 
 def gcd_division(a, b):
     if not a%b:
@@ -27,12 +77,21 @@ def solution(A, B):
     return result
 
 if __name__ == '__main__':
-    test_cases = (
+    tests = (
         (1, ([15, 10, 9], [75, 30, 5]) ),
         (2, ([7, 17, 5, 3], [7, 11, 5, 2]) ),
         (2, ([3, 9, 20, 11], [9, 81, 5, 13]) ),
     )
-    for expected, args in test_cases:
-        got = solution(*args)
-        print('result', expected, got)
-        assert(expected == got)
+    for expected, args in tests:
+        tic = time.perf_counter()
+        res = solution(*args)
+        toc = time.perf_counter()
+        
+        if expected is None:
+            print(f'SPEED-TEST {len(args[0])} args finished in {toc - tic:0.8f} seconds')
+            continue # This is just a speed test
+        print(f'ARGS produced {res} in {toc - tic:0.8f} seconds')
+        try:
+            assert(expected == res)
+        except AssertionError as e:
+            print(f'ERROR {args} produced {res} when {expected} was expected!')
